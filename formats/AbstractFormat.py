04d3b2b14b3c7b70b05a3f9a7484f8b9e00a2ed9
aOtt <a.ott@student.uni-tuebingen.de>
2018-06-17 19:47:15 +0200

    add abstract format and factory

---
+++ formats/AbstractFormat.py
@@ -0,0 +1,5 @@
+
+class AbsFormat:
+
+    def validate_file(self, file_path):
+        pass
---
+++ formats/FormatFactory.py
@@ -0,0 +1,10 @@
+from formats.fasta import Fasta
+
+
+def get_format(name):
+    if name == "fastaDna":
+        return Fasta("DNA")
+    if name == "fastaAA":
+        return Fasta("AA")
+
+    raise Exception("FormatFactory.get_format() - Error: Unknown Format identifier.")

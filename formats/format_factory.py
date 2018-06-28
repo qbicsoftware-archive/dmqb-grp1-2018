d1904404197adfd7c1b50c82c59eeeeac15add27
aOtt <a.ott@student.uni-tuebingen.de>
2018-06-28 17:20:11 +0200

    add placeholder docstrings

--- formats/format_factory.py
+++ formats/format_factory.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
 
-from formats.fasta.fasta import Fasta
+from formats.fasta import Fasta
 
 
 def available_formats():
+    """TODO DOC String"""
     return ["fasta-dna", "fasta-aa"]
 
 
 def get_format(name):
+    """TODO DOC String"""
     if name == "fasta-dna":
         return Fasta("DNA")
     if name == "fasta-aa":

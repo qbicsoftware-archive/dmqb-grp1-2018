674c23d6951bca694c959963364738bafe5d54c0
aOtt <a.ott@student.uni-tuebingen.de>
2018-07-03 00:06:54 +0200

    add helper function get_format from fileending

--- formats/format_factory.py
+++ formats/format_factory.py
@@ -7,7 +7,18 @@ def available_formats():
     return ["fasta-dna", "fasta-aa"]
 
 
-def get_format(name):
+def get_format_from_ending(file_ending):
+    """TODO"""
+    if file_ending == "fasta":
+        return "fasta-dna"
+    if file_ending == "fastq":
+        return "fastq"
+
+    # in case of unknown an ending return empty string.
+    return ""
+
+
+def get_validator(name):
     """TODO DOC String"""
     if name == "fasta-dna":
         return Fasta("DNA")

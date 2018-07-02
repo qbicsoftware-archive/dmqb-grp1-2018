72da4fbf6b5871cc046dd4eb696c6f49082c58e6
aOtt <a.ott@student.uni-tuebingen.de>
2018-07-03 00:07:31 +0200

    add return as example and error msg

--- formats/abstract_format.py
+++ formats/abstract_format.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+
 class AbsFormat:
     """TODO DOC String"""
 
     def validate_file(self, file_path):
         """TODO DOC String"""
-        pass
+        valid = False
+        return valid, "Error: Unimplemented validate_file function."

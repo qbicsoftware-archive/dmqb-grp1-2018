4ba0dbb4afcafe548b1f6c9e0976a89ff40118a3
aOtt <a.ott@student.uni-tuebingen.de>
2018-07-03 00:01:47 +0200

    rename folders without caps

--- tests/test_Fasta.py
+++ tests/test_Fasta.py
@@ -21,57 +21,49 @@ class TestFasta(unittest.TestCase):
     def test_valid(self):
         """TODO DOC String"""
         f = Fasta("DNA")
-        valid, filepath, msg = f.validate_file(PATH_VALID_FILES + "/fastaDNA/valid.fasta")
+        valid, msg = f.validate_file(PATH_VALID_FILES + "/fastaDNA/valid.fasta")
         self.assertTrue(valid)
-        self.assertEqual(filepath, PATH_VALID_FILES + "/fastaDNA/valid.fasta")
         self.assertEqual(msg, "")
 
         f = Fasta("AA")
-        valid, filepath, msg = f.validate_file(PATH_VALID_FILES + "/fastaAA/valid.fasta")
+        valid, msg = f.validate_file(PATH_VALID_FILES + "/fastaAA/valid.fasta")
         self.assertTrue(valid)
-        self.assertEqual(filepath, PATH_VALID_FILES + "/fastaAA/valid.fasta")
         self.assertEqual(msg, "")
 
     def test_wrong_coding(self):
         """TODO DOC String"""
         f = Fasta("DNA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/wrongCoding.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/wrongCoding.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaDNA/wrongCoding.fasta")
         self.assertEqual(msg, "Fasta: Character not allowed [O] in sequence at line: 2:10")
 
         f = Fasta("AA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/wrongCoding.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/wrongCoding.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaAA/wrongCoding.fasta")
         self.assertEqual(msg, "Fasta: Character not allowed [J] in sequence at line: 2:9")
 
     def test_empty_line(self):
         """TODO DOC String"""
         f = Fasta("DNA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/emptyLine.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/emptyLine.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaDNA/emptyLine.fasta")
         self.assertEqual(msg, "Fasta: Error empty line at line: 3")
 
         f = Fasta("AA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/emptyLine.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/emptyLine.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaAA/emptyLine.fasta")
         self.assertEqual(msg, "Fasta: Error empty line at line: 3")
 
     def test_double_header(self):
         """TODO DOC String"""
         f = Fasta("DNA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/doubleHeader.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaDNA/doubleHeader.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaDNA/doubleHeader.fasta")
         self.assertEqual(msg, "Fasta: Header without a sequence in line: 3")
 
         f = Fasta("AA")
-        valid, filepath, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/doubleHeader.fasta")
+        valid, msg = f.validate_file(PATH_CORRUPT_FILES + "/fastaAA/doubleHeader.fasta")
         self.assertFalse(valid)
-        self.assertEqual(filepath, PATH_CORRUPT_FILES + "/fastaAA/doubleHeader.fasta")
         self.assertEqual(msg, "Fasta: Header without a sequence in line: 3")
 
 
